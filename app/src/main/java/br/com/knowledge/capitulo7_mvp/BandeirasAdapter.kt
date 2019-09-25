02de60239c9f39411b40728754743e7b9276186e
enzo de carvalho teles <enzo.carvalho.teles@gmail.com>
2019-09-25 11:02:03 -0300

    update in the project

--- app/src/main/java/br/com/knowledge/capitulo7_mvp/BandeirasAdapter.kt
+++ app/src/main/java/br/com/knowledge/capitulo7_mvp/BandeirasAdapter.kt
@@ -1,36 +1,32 @@
 package br.com.knowledge.capitulo7_mvp
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.capitulo7_item.view.*
-import android.graphics.BitmapFactory
-import android.graphics.Bitmap
-import android.util.Base64
-import android.annotation.SuppressLint
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
-import androidx.core.content.ContextCompat
 import br.com.knowledge.R
-import br.com.knowledge.capitulo8_fragment.util.LogWrapperUtil
-import kotlinx.android.synthetic.main.capitulo2.view.*
+import kotlinx.android.synthetic.main.capitulo7_item.view.*
+
+
 
 
 class BandeieraAdapter(
     var listMessages: ArrayList<Bandeiras>,
     var context: Context
-):
-    RecyclerView.Adapter<VH>() {
+):RecyclerView.Adapter<VH>() {
+
+    companion object{
+        var mSelectedItem:Int = -1
+        var adapterB : BandeieraAdapter ?= null
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.capitulo7_item, parent, false)
+        adapterB = this
         val vh = VH(v)
-
-        vh.itemView.setOnClickListener {
-            val message = listMessages[vh.adapterPosition]
-        }
 
         return vh
 
@@ -42,23 +38,25 @@ class BandeieraAdapter(
     override fun onBindViewHolder(holder: VH, position: Int) {
         val bandeira = listMessages[position]
         holder.itemView.rdButton.text = bandeira.name
-        //holder.itemView.rdButton.setButtonDrawable(Drawable.createFromPath("https://pbs.twimg.com/profile_images/2227101468/enzo_400x400.jpg"))
-
-
-    }
-
-    fun StringToBitMap(encodedString: String): Bitmap? {
-        try {
-            val encodeByte = Base64.decode(encodedString, Base64.DEFAULT)
-            return BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.size)
-        } catch (e: Exception) {
-            e.message
-            return null
-        }
+        holder.itemView.rdButton.isChecked = (position == mSelectedItem)
+        holder.loadData()
 
     }
 
 }
 
-class VH(itemView: View) : RecyclerView.ViewHolder(itemView)
+class VH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun loadData() {
+        var listener: View.OnClickListener = View.OnClickListener {
+            BandeieraAdapter.mSelectedItem = adapterPosition
+            BandeieraAdapter.adapterB!!.notifyDataSetChanged()
+
+        }
+        itemView.rdButton.setOnClickListener(listener)
+        itemView.setOnClickListener(listener)
+
+    }
+}
+
 
